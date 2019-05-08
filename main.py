434e77b382116497693af31c95370dc897ab109b
yta-git <27941112+yta-git@users.noreply.github.com>
2019-05-08 18:23:59 +0900

    init

---
+++ main.py
@@ -0,0 +1,34 @@
+from sys import argv
+from os import system
+from time import sleep
+import MeCab
+
+speed = 0.05
+m = MeCab.Tagger('-Ochasen')
+
+with open(argv[1]) as file:
+    text = m.parse(file.read()).split('\n')
+
+total = len(text)
+counter = 0
+
+system('clear')
+print(f'{total} words, {int(speed * total)} seconds')
+print('ready...')
+sleep(3)
+
+for line in  text:
+    
+    system('clear')
+    
+    if line:
+        word = line.split()[0]
+
+    if word != 'EOS':
+        print(word)
+        print(f'{total - counter} words\n{int(speed * (total - counter))} seconds remines')
+
+    counter = counter + 1
+    sleep(speed)
+    
+
